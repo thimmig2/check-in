3bf150a2bd277d86ee2d59c8aee0e1534bff7a88
Sebastian Thimmig <thimmig.2@osu.edu>
2014-01-11 18:29:25 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AkpsiPoints::Application.config.secret_key_base = '0e109b12b145c685726b2c053489489483e078c85be8421c39e9823e378fe6bbfdd7e0d70083476e2daf7db51d55fffbdc7bb93cab2b523d6941d59182e44713'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AkpsiPoints::Application.config.session_store :cookie_store, key: '_akpsi-points_session'
