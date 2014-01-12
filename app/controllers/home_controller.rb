933f69f1ea36ec2eee64ad8ed498c234a248371a
Sebastian Thimmig <thimmig.2@osu.edu>
2014-01-12 20:06:12 +0000

    modified points method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,5 +9,15 @@ class HomeController < ApplicationController
       format.html  # index.html.erb
     end
   end
+
+  private
+  
+  def calculatePoints(checkins)
+    @points = 0
+    checkins.each do |checkin|
+      @points += 25
+    end
+    @points
+  end
   
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,11 +1,3 @@
 module HomeHelper
-  
-  def calculatePoints(checkins)
-    @points = 0
-    checkins.each do |checkin|
-      @points += 25
-    end
-    @points
-  end
     
 end
