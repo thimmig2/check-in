da9c42585018f150ac9acfbcd5952289f92a2402
Sebastian Thimmig <thimmig.2@osu.edu>
2014-01-12 20:01:35 +0000

    additional configuration

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,8 +1,8 @@
 module HomeHelper
   
-  def calculatePoints(@checkins)
+  def calculatePoints(checkins)
     @points = 0
-    @checkins.each do |checkin|
+    checkins.each do |checkin|
       @points += 25
     end
     @points
