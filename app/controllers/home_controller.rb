b0c05b96c7cdb7575a16990867e8c79f32d0831a
Sebastian Thimmig <thimmig2@gmail.com>
2014-01-13 21:49:53 +0000

    Modified points function, minor ui changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -16,8 +16,19 @@ class HomeController < ApplicationController
   
   def calculatePoints(checkins)
     @points = 0
+    @types = Hash.new
     checkins.each do |checkin|
-      @points += 25
+      
+      @types[checkin.event.type_id] = 0 unless @types.has_key? checkin.event.type_id
+      
+      if checkin.event.type_id == "Distinction"
+        @points += 60
+      else
+        @points += 25 - (5 * @types[checkin.event.type_id]) unless @types[checkin.event.type_id] == 3
+      end
+      
+      @types[checkin.event.type_id] += 1;
+        
     end
     @points
   end
